2baa96caa082f603140dfb4e989d202375ccf803
Andrew George Issac <andrewgeorgeissac@gmail.com>
2020-07-09 12:30:26 +0400

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -7,26 +7,38 @@ from PIL import Image
 from fpdf import FPDF
 from docx2pdf import convert
 from moviepy.editor import *
+import time
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
+        global central_widget
         super(MainWindow, self).__init__(parent)
-        self.central_widget = QStackedWidget()
-        self.setCentralWidget(self.central_widget)
+        central_widget = QStackedWidget()
+        self.setCentralWidget(central_widget)
         self.setWindowTitle('DigiZoc_v1.0')
         self.resize(900,600)
-        login_widget = LoginWidget(self)
-        #login_widget.button.clicked.connect(self.login)
-        self.central_widget.addWidget(login_widget)
+        login_widget = Home(self)
+        central_widget.addWidget(login_widget)
     def login(self):
         logged_in_widget = LoggedWidget(self)
-        self.central_widget.addWidget(logged_in_widget)
-        self.central_widget.setCurrentWidget(logged_in_widget)
+        central_widget.addWidget(logged_in_widget)
+        central_widget.setCurrentWidget(logged_in_widget)
 
-class Welcome(QWidget): #welcome_page
-    def __init__(self, parent=None):
-        super(Welcome).__init__(parent)
-        pass
+
+
+class Home(QWidget): #welcome_page
+    def __init__(self,parent=None):
+        super(Home, self).__init__(parent)
+        layout = QHBoxLayout()
+        self.button = QPushButton('Login')
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+        self.show()
+        #MainWindow.login(self)
+
+
+
+
 
 class LoginWidget(QWidget):#home_page
     def __init__(self, parent=None):
