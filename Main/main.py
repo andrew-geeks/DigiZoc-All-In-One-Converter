b68b538109f97e64ed0dcf8352778bb45673137e
Andrew George Issac <andrewgeorgeissac@gmail.com>
2020-07-10 17:15:16 +0400

    main

--- Main/main.py
+++ Main/main.py
@@ -17,10 +17,11 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
         self.setWindowTitle('DigiZoc_v1.0')
         self.resize(900,600)
+        self.setWindowIcon(QIcon('files/logo.ico'))
         #self.setMaximumHeight(600)
         #self.setMaximumWidth(900)
         self.setStyleSheet("background-color: white;")
-        login_widget = Home(self)
+        login_widget = Welcome(self)
         central_widget.addWidget(login_widget)
         
     def back_to_home(self):#back_function
@@ -39,6 +40,7 @@ class MainWindow(QMainWindow):
 class Home(QWidget): #Home_page
     def __init__(self,parent=None):
         global frame
+        global framelayout
         super(Home, self).__init__(parent)
         welcome=QLabel('All In One Converter',self)
         welcome.move(7,7)
@@ -56,36 +58,44 @@ class Home(QWidget): #Home_page
         b3.move(60,380)
         b3.clicked.connect(self.visual_page)
         frame=QFrame(self)
-        frame.setLayout
         frame.hide()
-        frame.deleteLater()
+        framelayout=QVBoxLayout()
     def image_page(self): 
         try:
             frame.show()
         except:
             pass
-        layout=QVBoxLayout()
+        try:
+            for i in reversed(range(framelayout.count())): 
+                framelayout.itemAt(i).widget().deleteLater()
+        except:
+            pass
         frame.setFixedSize(400,600)
         frame.move(500,0)
         frame.setStyleSheet("background-color: SlateBlue")
-        frame.setLayout(layout)
+        frame.setLayout(framelayout)
         heading=QLabel('Image Converter')
-        heading.setStyleSheet("QLabel {font: 15pt Times}")
-        layout.addWidget(heading)
+        heading.setStyleSheet("QLabel {font: 25pt Times}")
+        framelayout.addWidget(heading)
         b4=QPushButton('Jpg to Png')
-        b4.setStyleSheet("background-color:black; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
-        layout.addWidget(b4)
+        b4.setStyleSheet("background-color:green; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
+        framelayout.addWidget(b4)
         b4=QPushButton('Jpg to pdf')
-        b4.setStyleSheet("background-color:black; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
-        layout.addWidget(b4)
+        b4.setStyleSheet("background-color:green; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
+        framelayout.addWidget(b4)
         b5=QPushButton('Png to Ico')
-        b5.setStyleSheet("background-color:black; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
-        layout.addWidget(b5)
-        #b4.move(5,7)
+        b5.setStyleSheet("background-color:green; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
+        framelayout.addWidget(b5)
+        
 
     def visual_page(self):
         try:
             frame.show()
+        except:
+            pass
+        try:
+             for i in reversed(range(framelayout.count())): 
+                framelayout.itemAt(i).widget().deleteLater()
         except:
             pass
         frame.setFixedSize(400,600)
@@ -96,22 +106,30 @@ class Home(QWidget): #Home_page
             frame.show()
         except:
             pass
+        try:
+            for i in reversed(range(framelayout.count())): 
+                framelayout.itemAt(i).widget().deleteLater()
+        except:
+            pass
         frame.setFixedSize(400,600)
         frame.move(500,0)
         frame.setStyleSheet("background-color: SlateBlue")
+        heading=QLabel('Document Converter')
+        heading.setStyleSheet("QLabel {font: 25pt Times}")
+        framelayout.addWidget(heading)
+        b1=QPushButton('Docx to Pdf')
+        b1.setStyleSheet("background-color:green; font: bold 14px; min-width: 7em; min-height: 2em; border-radius: 10px;padding: 6px; color:white")
+        framelayout.addWidget(b1)
 
 
 
 
-class LoginWidget(QWidget): #Anticipatory
+class Welcome(QWidget): #Anticipatory
     def __init__(self, parent=None):
-        super(LoginWidget, self).__init__(parent)
-        layout = QHBoxLayout()
-        self.button = QPushButton('Login')
-        layout.addWidget(self.button)
-        self.setLayout(layout)
-        #self.button.clicked
-        self.button.clicked.connect(self.parent().login)
+        super(Welcome, self).__init__(parent)
+        #self.setWindowIcon(QIcon('files/logo.ico'))
+        
+
         # you might want to do self.button.click.connect(self.parent().login) here
 
 
